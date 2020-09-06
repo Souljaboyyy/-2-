a64c076df8490f7720f155f1c6aa57929f1af0e8
Souljaboyyy <makarkarpovski@mail.ru>
2020-09-06 14:26:30 +0500

    Task 1 Strings VS-2015

--- Task1cpp/Source.cpp
+++ Task1cpp/Source.cpp
@@ -4,25 +4,54 @@
 using namespace std;
 
 int main() {
-	int i = 0,n=0;string mass[256];
 	fstream inOut;
-	std::string str;
+	string str, line;
+	int CountStrings = 0, i = 0;
 	inOut.open("file.txt", ios::in);
-	while (inOut) {
-		std::getline(inOut, str);
-		n++;
+	while (!inOut.eof()) {
+		getline(inOut, str);
+		CountStrings++;
 	}
-	//	cout << n << endl;
+	inOut.seekg(0, ios_base::beg);
+	inOut.close();
+	inOut.open("file.txt", ios::in);
+	cout << "Your strings : " << endl;
+	string* massLines = new string[CountStrings];
+	for (i; i < CountStrings; i++) {
+		getline(inOut, massLines[i]);
+		cout << massLines[i] << endl;
+	}
+	cout << endl;
+	for (i = 0; i < CountStrings; i++)
+		for (int j = 0; j < CountStrings - 1; j++)
+			if (massLines[j] > massLines[j + 1]) {
+				std::swap(massLines[j], massLines[j + 1]);
+			}
 	
-	while (inOut) {
-		for (i; i < n; i++) {
-			std::getline(inOut, str);
-			mass[i] = str;
-			i++;
+	int count = 0;
+	for (i = 0; i < CountStrings-1 ; i++)
+		if (massLines[i] != massLines[i + 1])
+			count++;
+	count++;
+	string* NewMassLines = new string[count];
+	for (int j = 0, k = 0; k < CountStrings - 1; k++)
+	{
+		if (massLines[k] != massLines[k + 1])
+		{
+			NewMassLines[j] = massLines[k];
+			++j;
+		}
+		if (k + 1 == CountStrings - 1)
+		{
+			NewMassLines[j] = massLines[k+1];
 		}
 	}
-	for (i = 0; i < n; i++)
-		cout << mass[i] << endl;
+	cout << endl;
+	cout << "You want : " << endl;
+	for (i = 0; i < count; i++)
+		cout << NewMassLines[i] << endl;
+	delete[] massLines;
+		
 	system("pause");
 	return 0;
 }
